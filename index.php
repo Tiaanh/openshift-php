f510de7a87f6b1f37cc21e73d7a37a50a61c8bb9
Tiaanh <tiaanh@gmail.com>
2018-03-21 22:01:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello, I'm running on container </p>'; ?> 
+ <?php echo '<p>Hello, I am running on container </p>'; ?> 
  <?php echo gethostname() ; ?> 
  </body>
 </html>
