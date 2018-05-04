8ef1b6e06308c957caaf48be8e94a1eb9cdd2ac4
Tiaanh <tiaanh@gmail.com>
2018-05-04 09:40:25 +0000

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   for ($x = 0; $x <= 1000; $x++) {
   } 
   $hostname = gethostname();
-  print "<p>I am version 7 and running on container - $hostname</p>" ; 
+  print "<p>I am version 8 and running on container - $hostname</p>" ; 
  ?> 
  </body>
 </html>
